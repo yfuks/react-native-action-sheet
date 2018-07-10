301a3f2117da63aee2e0276f2f6c041e22d7ef88
Bruno Lemos <brunolemos@users.noreply.github.com>
2018-07-10 01:02:51 -0300

    Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module '@yfuks/react-native-action-sheet' {
+  import { ActionSheetIOSStatic } from 'react-native'
+
+  const ActionSheetIOS: ActionSheetIOSStatic
+  export default ActionSheetIOS
+}
